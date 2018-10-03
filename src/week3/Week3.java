5c00df1fdde4b5b83c4c9fdf412417bb2dde11a7
PhamDanhChien <43428098+PhamDanhChien@users.noreply.github.com>
2018-10-04 00:17:34 +0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -4,12 +4,20 @@ public class Week3 {
 
     public static int max(int m, int n) {
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+	    if(m>n) return m;
+	    else return n;
     }
 
     public static int minOfArray(int[] array) {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+	    int n = array.length;
+    	int min = array[0];
+    	
+        for(int i=1;i<n;i++){
+        	if(min > array[i]) min = array[i];
+        }
+        
+        return min;
     }
 
     /**
@@ -20,6 +28,20 @@ public class Week3 {
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+	    if(weight <= 0 || height <= 0) return null;
+    	
+    	double BMI;
+    	BMI = weight/(height*height);
+    	String ketqua = new String(); 
+    	
+    	if(BMI < 18.5) ketqua = "Thieu can";
+    	else if(18.5 <= BMI && BMI < 23) ketqua = "Binh thuong";
+    	else if(23 <= BMI && BMI < 25) ketqua = "Thua can";
+    	else ketqua = "Beo phi";
+
+    		
+    	System.out.println(ketqua);
+    	
+    	return ketqua;
     }
 }
