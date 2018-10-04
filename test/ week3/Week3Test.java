8749137fc060a63025889a37da134da681880518
PhamDanhChien <43428098+PhamDanhChien@users.noreply.github.com>
2018-10-04 09:47:11 +0700

    Update Week3Test.java

--- test/ week3/Week3Test.java
+++ test/ week3/Week3Test.java
@@ -10,11 +10,11 @@ public class Week3Test {
 
 	@Test	
 	void testMax() {
-		assertEquals(4, week3.max(2, 4));
-		assertEquals(3, week3.max(3, -19));
-		assertEquals(10, week3.max(0, 10));
-		assertEquals(5, week3.max(5, 5));
-		assertEquals(1999, week3.max(1999, 11));
+		assertEquals(4, Week3.max(2, 4));
+		assertEquals(3, Week3.max(3, -19));
+		assertEquals(10, Week3.max(0, 10));
+		assertEquals(5, Week3.max(5, 5));
+		assertEquals(1999, Week3.max(1999, 11));
 	}
     // TODO: Viết 5 testcase cho phương thức minOfArray()
 	
@@ -27,22 +27,22 @@ public class Week3Test {
 		int mang5[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 		
 		
-		assertEquals(5, week3.minOfArray(  mang1  ) );
-		assertEquals(3, week3.minOfArray(  mang2  ) );
-		assertEquals(2, week3.minOfArray(  mang3  ) );
-		assertEquals(-1, week3.minOfArray(  mang4  ) );
-		assertEquals(1, week3.minOfArray(  mang5  ) );
+		assertEquals(5, Week3.minOfArray(  mang1  ) );
+		assertEquals(3, Week3.minOfArray(  mang2  ) );
+		assertEquals(2, Week3.minOfArray(  mang3  ) );
+		assertEquals(-1, Week3.minOfArray(  mang4  ) );
+		assertEquals(1, Week3.minOfArray(  mang5  ) );
 		
 	}
     // TODO: Viết 5 testcase cho phương thức calculateBMI()
 	
 	@Test
 	void testBmi() {
-		assertEquals(null, week3.calculateBMI(-10, 1.67));
-		assertEquals("Thiếu cân", week3.calculateBMI(45, 1.7));
-		assertEquals("Bình thường", week3.calculateBMI(55, 1.67));
-		assertEquals("Thừa cân", week3.calculateBMI(60, 1.60));
-		assertEquals("Béo phì", week3.calculateBMI(75, 1.55));
+		assertEquals(null, Week3.calculateBMI(-10, 1.67));
+		assertEquals("Thiếu cân", Week3.calculateBMI(45, 1.7));
+		assertEquals("Bình thường", Week3.calculateBMI(55, 1.67));
+		assertEquals("Thừa cân", Week3.calculateBMI(60, 1.60));
+		assertEquals("Béo phì", Week3.calculateBMI(75, 1.55));
 		
 	}
 }
