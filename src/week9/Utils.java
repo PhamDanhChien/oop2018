d7b44395e0aba980824a26cb7ad5554fa9735819
PhamDanhChien <43428098+PhamDanhChien@users.noreply.github.com>
2018-11-08 02:37:53 +0700

    Update Utils.java
    dadajdbasdba

--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -8,23 +8,23 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class Utils {
-  public static String readContentFromFile(String path) {
+	public static String readContentFromFile(String path) {
 		
 		try {  
             
 			FileReader fr = new FileReader(new File(path));
 			BufferedReader br = new BufferedReader(fr);
 			
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                    System.out.println("line data: " + line);
-            }
+            		String line = null;
+            		while ((line = br.readLine()) != null) {
+                	    System.out.println("line data: " + line);
+            		}
        
-            br.close();
-        }
+           		br.close();
+       	 	}
 		catch (IOException e) {
-        	System.out.println(e);
-        }
+        		System.out.println(e);
+        	}
 		
 		return null;
 	
@@ -42,8 +42,8 @@ public class Utils {
 			br.close();
 		}
 		catch (IOException e) {
-        	System.out.println(e);
-        }
+        		System.out.println(e);
+        	}
 	}
 	
 	
@@ -56,35 +56,36 @@ public class Utils {
 			br.close();
 		}
 		catch (IOException e) {
-        	System.out.println(e);
-        }
+        		System.out.println(e);
+       	 	}
 	}
 	
 	public static File findFileByName(String folderPath, String fileName) {
 		File dir = new File(folderPath);
 		String[] children = dir.list();
 		
-	      if (children == null) {
-	         System.out.println("day khong phai la thu muc hoac khong ton tai");
-	      }
-	      else {
-	    	 boolean fi = false;
+	      	if (children == null) {
+	        	 System.out.println("day khong phai la thu muc hoac khong ton tai");
+			return null;
+	      	}
+	      	else {
+	    	 	boolean fi = false;
 	    	 
-	         for (int i = 0; i < children.length; i++) {
+			for (int i = 0; i < children.length; i++) {
 	       
-	             if( children[i].equals(fileName) == true) {
-	            	 fi = true;
-	            	 System.out.println("da tim thay file");
-	            	 break;
-	             }
-	         }
+	            		 if( children[i].equals(fileName) == true) {
+	            	 		fi = true;
+	            			System.out.println("da tim thay file");
+	            			break;
+	            		 }
+	        	}
 	         
-	         if(fi == false) {
-	        	 System.out.println("khong tim thay file"); 
-	        	 return null;
-	         }
+	         	if(fi == false) {
+	        		 System.out.println("khong tim thay file"); 
+	        		 return null;
+	        	}
 	          
-	      }
+	     	}
 	      
 	      String path = folderPath + "/" + fileName;
 	      File kq = new File(path);
